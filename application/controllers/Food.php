b84b0cc94613dd1ccf66e44da6cb59f7d4231eda
din1030 <din1030@gmail.com>
2016-03-20 17:15:44 +0800

    update

--- application/controllers/Food.php
+++ application/controllers/Food.php
@@ -36,7 +36,7 @@ class Food extends CI_Controller {
         } else {
         	$data = array(
 		        'food' => $this->input->post('food'),
-		        'food_quantity' => $this->input->post('food_quantity'),
+		        'quantity' => $this->input->post('food_quantity'),
 		        'date' => $this->input->post('food_date'),
 		        'time' => $this->input->post('food_time')
 			);
