6c19e283f313c94cac31fa6b7b5b1d5bbb030bf9
din1030 <din1030@gmail.com>
2016-03-21 18:06:27 +0800

    update header

--- application/views/header.php
+++ application/views/header.php
@@ -1,6 +1,9 @@
 <html>
     <head>
     	<meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
     	<meta name="author" content="鄭嘉鼎">
     	<meta property="description" content="Go Fight Win 每日記錄" />
     	<meta property="og:title" content="Go Fight Win 每日記錄" />
